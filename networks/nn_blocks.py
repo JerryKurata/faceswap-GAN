448ddb44fee0a78f823d3ad98be5b8a3d12954a8
jerrykur <jerryk@insteptech.com>
2019-09-08 11:00:29 -0700

    added import for keras regulatizers

--- networks/nn_blocks.py
+++ networks/nn_blocks.py
@@ -5,7 +5,7 @@ from .GroupNormalization import GroupNormalization
 from .pixel_shuffler import PixelShuffler
 from .custom_layers.scale_layer import Scale
 from .custom_inits.icnr_initializer import icnr_keras
-from keras import regu
+from keras import regularizers
 import tensorflow as tf
 import keras.backend as K
 
